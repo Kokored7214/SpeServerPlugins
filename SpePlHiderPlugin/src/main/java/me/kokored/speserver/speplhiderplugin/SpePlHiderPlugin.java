1afe0a58d622e17aa30671d6422ab4eb79fc3dff
Kokored7214 <kokored69166434@gmail.com>
2021-01-21 12:02:25 +0800

    SpePlHiderPlugin - 創建插件

---
+++ SpePlHiderPlugin/src/main/java/me/kokored/speserver/speplhiderplugin/SpePlHiderPlugin.java
@@ -0,0 +1,17 @@
+package me.kokored.speserver.speplhiderplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpePlHiderPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
