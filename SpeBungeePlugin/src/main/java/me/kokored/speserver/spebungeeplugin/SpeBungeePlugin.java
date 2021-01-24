f903bbce5593a1971ceebda23137580d478b247d
Kokored7214 <kokored69166434@gmail.com>
2021-01-24 12:03:47 +0800

    SpeBungeePlugin - 新增項目

---
+++ SpeBungeePlugin/src/main/java/me/kokored/speserver/spebungeeplugin/SpeBungeePlugin.java
@@ -0,0 +1,16 @@
+package me.kokored.speserver.spebungeeplugin;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class SpeBungeePlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
