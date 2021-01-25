245b4d6d611f96095cc171d27f543c2156ad07c8
Kokored7214 <kokored69166434@gmail.com>
2021-01-25 18:54:14 +0800

    SpeEntityControlPlugin - 伺服器實體管理插件Dev

---
+++ SpeEntityControlPlugin/src/main/java/me/kokored/speserver/speentitycontrolplugin/SpeEntityControlPlugin.java
@@ -0,0 +1,17 @@
+package me.kokored.speserver.speentitycontrolplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpeEntityControlPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
