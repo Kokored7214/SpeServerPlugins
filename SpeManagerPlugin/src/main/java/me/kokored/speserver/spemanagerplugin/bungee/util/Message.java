57637e4085f49264e7d51eb727775b135ed96bc6
Kokored7214 <kokored69166434@gmail.com>
2021-01-29 19:10:11 +0800

    新增信息輔助程序

---
+++ SpeManagerPlugin/src/main/java/me/kokored/speserver/spemanagerplugin/bungee/util/Message.java
@@ -0,0 +1,63 @@
+package me.kokored.speserver.spemanagerplugin.bungee.util;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+public class Message {
+
+    public static void consoleLog(String levels, String message) {
+
+        String prefix = "[SpeManagerPlugin-BC] ";
+
+        switch (levels) {
+            case "info":
+                System.out.println(prefix + "[Info] " + message);
+                break;
+            case "warning":
+                System.out.println(prefix + "[Warning] " + message);
+                break;
+            case "error":
+                System.out.println(prefix + "[Error] " + message);
+                break;
+            case "sql_info":
+                System.out.println(prefix + "[MySQL-Info] " + message);
+                break;
+            case "sql_warning":
+                System.out.println(prefix + "[MySQL-Warning] " + message);
+                break;
+            case "sql_error":
+                System.out.println(prefix + "[MySQL-Error] " + message);
+                break;
+        }
+
+    }
+
+    public static void sendAdminMessage(String message) {
+
+        for (ProxiedPlayer online_players : ProxyServer.getInstance().getPlayers()) {
+
+            if (online_players.hasPermission("spe.managerplugin.admin")) {
+
+                ProxiedPlayer admin = online_players;
+
+                admin.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', message)));
+
+            }
+
+        }
+
+    }
+
+    public static void sendGlobalMessage(String message) {
+
+        for (ProxiedPlayer online_players : ProxyServer.getInstance().getPlayers()) {
+
+            online_players.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', message)));
+
+        }
+
+    }
+
+}
