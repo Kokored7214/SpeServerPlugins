2b3dd9a237c15c59a4704517e4306f43a4eb0257
Kokored7214 <kokored69166434@gmail.com>
2021-01-29 19:09:48 +0800

    獲取錯誤代碼

---
+++ SpeManagerPlugin/src/main/java/me/kokored/speserver/spemanagerplugin/bungee/util/ErrorCode.java
@@ -0,0 +1,12 @@
+package me.kokored.speserver.spemanagerplugin.bungee.util;
+
+public class ErrorCode {
+
+    static String smp = "smpbc_";
+
+    //資料庫尚未設定或未連綫
+    public static String sql_100() {
+        return smp + "sql_100";
+    }
+
+}
