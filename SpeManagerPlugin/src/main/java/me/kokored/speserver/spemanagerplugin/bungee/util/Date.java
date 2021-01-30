ca3111ca6a5510200cad539d926017177013ddd5
Kokored7214 <kokored69166434@gmail.com>
2021-01-30 17:47:48 +0800

    SMP - 新增自定格式日期獲取方式

--- SpeManagerPlugin/src/main/java/me/kokored/speserver/spemanagerplugin/bungee/util/Date.java
+++ SpeManagerPlugin/src/main/java/me/kokored/speserver/spemanagerplugin/bungee/util/Date.java
@@ -11,4 +11,11 @@ public class Date {
         return format.format(now);
     }
 
+    //取得現在日期及時間(自定格式)
+    public static String getDate(String format) {
+        java.util.Date now = new java.util.Date();
+        SimpleDateFormat format1 = new SimpleDateFormat(format);
+        return format1.format(now);
+    }
+
 }
